9a5e43660fb0dda81024e31341bf239bd4c7dd30
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-04-18 22:36:33 +0100

    Find root path

--- analysis/file_locator.rb
+++ analysis/file_locator.rb
@@ -13,11 +13,24 @@ module Analysis
     end
 
     def call
-      r = IO.popen("find . ! -iwholename '*.git*' -type f | tr '\n' ','", "r+")
-      arr = r.readlines
-      r.close
+      array = uri.split("/")
+      array.pop
+      root_path = nil
 
-      arr.first.split(",")
+      loop do
+        break if array.empty?
+
+        root_path = array.join("/")
+        root_path = "#{root_path}/.git"
+
+        terminal = IO.popen("[ -d /#{root_path} ] && echo true")
+          break if terminal.gets&.strip == "true"
+        terminal.close
+
+        array.pop
+      end
+
+      puts root_path
     end
   end
 end
